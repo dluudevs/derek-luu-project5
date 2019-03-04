0b1520d2bcded12c962386c7d1139a98fae61907
Derek <derekluu90@gmail.com>
2019-03-03 22:55:06 -0500

    changed API call, sorted by release date

--- src/App.js
+++ src/App.js
@@ -7,27 +7,27 @@ import './App.css';
 //import is a two way street - you only have to import once 
     //you also import any modules that file imported
 
-const apiKey = '1707fe563bfae1485271371502c54d58b9ec022d';
-const apiURL_games = 'https://www.giantbomb.com/api/games';
+const apiURL_games = 'http://www.gamespot.com/api/games'
+const apiURL_releases = 'http://www.gamespot.com/api/releases/'
+const apiKey = '7c8c72eb22a85289937160cd744b4ef17f79669d';
 const proxyURL = 'https://proxy.hackeryou.com'
 
 class App extends Component {
   
   constructor(){
     super();
+
     this.state = {
+      
       userQuery: '',
-      results: [
-                  {
-                    image: {},
-                    original_release_date: "1990-03-09 09:00"
-                  }
-                ]
+      results: [],
+      initialResults: []
+      //results is responsible for passing data to components to render
       //return error while other components are waiting for data, create an init method that appends to screen when promise is fulfiled and stores the array in results
     }
   }
 
-  componentWillMount(){
+  componentDidMount(){
 
     axios({
       url: proxyURL,
@@ -41,24 +41,24 @@ class App extends Component {
         params: {
           api_key: apiKey,
           format: 'json',
-          filter: `name:zelda,platforms:94|146|145|157`,
+          filter: 'release_date:2019-12-31'
           // Filter will find games that have at least one of values in the query
-          // sort: 'original_release_date:desc,'
           //to get rid of unreleased games, have another method that filters out games that have null in origial_release_date
-          limit: 5
         }
       }
     })
     .then((object) => {
       const searchResults = object.data.results;
-      //release date
       console.log(searchResults);
       this.setState({
         results: searchResults
-      });
-    })
-
+      })
+    });
   }
+
+
+  // filterReleasedGames = gamesArray => {
+  // }
   
   handleChange = event => {
     this.setState({
@@ -70,12 +70,6 @@ class App extends Component {
     event.preventDefault();
     this.findGames(this.state.userQuery);
   }
-
-  //create method to find platform and only display the name of the platforms i asked for 
-  findPlatform = (e) => {
-    e.preventDefault();
-  }
-
 
   //call to find games
   //TODO: sort to show the newest platforms first
@@ -92,56 +86,34 @@ class App extends Component {
         params: {
           api_key: apiKey,
           format: 'json',
-          filter: `name:${this.state.userQuery},platforms:94|146|145|157`,
+          filter: `name:${this.state.userQuery}`,
           // Filter will find games that have at least one of values in the query
-          // sort: 'original_release_date:desc',
-          //to get rid of unreleased games, have another method that filters out games that have null in origial_release_date
-          limit: 5
+          sort: 'release_date:desc',
         }
       }
     })
-      .then((object) => {
-        const searchResults = object.data.results;
-        //release date
-        console.log(searchResults);
-        this.setState({
-            results: searchResults
-        });
-      })
+    .then((object) => {
+      const searchResults = object.data.results;
+      //release date
+      console.log(searchResults);
+      this.setState({
+          results: searchResults
+      });
+    })
+    //include error handling
   }
 
-  render() {
+  render(){
     return (
       <div>
         <Header handleChange={this.handleChange} handleSubmit={this.handleSubmit} userQuery={this.state.userQuery}/>
-        <Body results={this.state.results} findPlatform={this.findPlatform}/>
+        <Body initialResults={this.state.initialResults} results={this.state.results} dateCheck={this.dateCheck}/>
       </div>
     );
   }
-  //bind this.state.userQuery to SearchBar's input
-
-
-  
 }
 
-// Platform - dont print platform for now. print genre instead. this is a stretchgoal after firebase
-//go into games (array)
-  //iterate through each game (object)
-  //go inside the platform property (array)
-    //iterate through that array, if the ID is equal to one of the ones in my array. print, else iterate to the next one
-    //i believe all you need to do is get inside the platforms array and print
-    //maybe create a new array of the values that match another array and print those    
-
 export default App;
-
-//Testing area
-
-
-//platform id
-  //PC: 94
-  //ps4: 146
-  //xone: 145
-  //Switch: 157
 
 
 
--- src/Body.js
+++ src/Body.js
@@ -1,31 +1,106 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Body = (props) => {
-    // let releaseDate = props.results[0].original_release_date.substring(0, 10);
-    return(
-            <main className="wrapper">
-                   { 
-                       props.results.map(game => {
-                           let releaseDate = game.original_release_date.substring(0, 10);
-                            return(
+class Body extends Component {
 
-                                <div className="card flex_row" key={game.id}>
-                                    <div className="img_container">
-                                        <img src={game.image.original_url} alt={`Cover of ${game.name}`}/>
-                                    </div>
-                                    <div className="text_container">
-                                        <h2>{game.name}</h2>
-                                        <h3>{`Release Date: ${releaseDate}`}</h3> 
-                                        <p className="description">{game.deck}</p> 
-                                    </div>
-                                </div>
+    constructor(){
+        super();
 
-                            )
-                       })
-                    }
-            </main>
+        //set today's date; use date to filter results for games that are not released
+        let today = new Date();
+        let dd = today.getDate();
+        let mm = today.getMonth() + 1; //January is 0!
+        let yyyy = today.getFullYear();
+
+        if (dd < 10) {
+            dd = '0' + dd;
+        }
+
+        if (mm < 10) {
+            mm = '0' + mm;
+        }
+
+        today = yyyy + '-' + mm + '-' + dd
+        //Source: https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
+
+        this.state = {
+            date: today,
+        }
+    }
+
+    showReleasedGames = () => {
+        const gameList = this.props.results.map(game => {
+
+            let dateString = game.release_date.substring(0, 10);
+            let gameDate = new Date(dateString);
+            let stateDate = new Date(this.state.date)
+
+            if (game.image && gameDate < stateDate && game.description !== '') {
+                return (
+                    <div className="card flex_row" key={game.id}>
+                        <div className="img_container">
+                            <img src={game.image.original} alt={`Cover of ${game.name}`} />
+                        </div>
+                        <div className="text_container">
+                            <h2>{game.name}</h2>
+                            <h3>{`Release Date: ${dateString}`}</h3>
+                            <p className="description">{game.description}</p>
+                        </div>
+                    </div>
+                );
+            }
+        })
+    }
+
+    showGames = () => {
+
+        const gameList = this.props.results.map(game => {
+
+            let dateString = game.release_date.substring(0, 10);
+            let gameDate = new Date(dateString);
+            let stateDate = new Date(this.state.date)
+
+            if(game.image && gameDate < stateDate && game.description !== ''){
+                return (    
+                    <div className="card flex_row" key={game.id}>
+                        <div className="img_container">
+                            <img src={game.image.original} alt={`Cover of ${game.name}`} />
+                        </div>
+                        <div className="text_container">
+                            <h2>{game.name}</h2>
+                            <h3>{`Release Date: ${dateString}`}</h3>
+                            <p className="description">{game.description}</p>
+                        </div>
+                    </div>
+                );
+            }
+        })
+
+        return (<div className="gameList">{gameList}</div>)
+    };
+
+    //if initial.length > 1, render initial load
+        //else load userquery
+
+        //on componentdidmount, clear initial to empty 
+
+
+    render(){
+        return(
+                <main className="wrapper">
+                        { 
+                            this.props.results.length > 0 ?
+                            this.showGames() : 
+                            <p>Loading Games...</p>
+                        }
+                </main>
         ); 
-}
+    }
+
+}//Component
+
+//render the inital load
+//componentDidUpdate, if the searchquery change, on click run a different render method
+
 // results may return more than 5, but append 5 at a time. use a condition so it isnt trying to append if there is less than 5 results
 //filter results for only items with a value in original_game_rating
 
